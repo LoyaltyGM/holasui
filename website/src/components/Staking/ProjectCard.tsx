93680f3c3d4117339b993bdc1b878d992fc9cfbc
igorlebedev <igorlebedev228@gmail.com>
2023-07-28 20:56:58 +0700

    rewrite card design

--- website/src/components/Staking/ProjectCard.tsx
+++ website/src/components/Staking/ProjectCard.tsx
@@ -12,49 +12,43 @@ export const ProjectCard = ({
   claimPointsFunction,
 }: IProjectCard) => {
   return (
-    <div className="h-[30rem] gap-2 rounded-xl border-2 border-[#F6F6F6] bg-[#FFFFFF] px-2 py-4 md:flex md:h-60 md:p-4 md:py-8">
-      <div className="flex flex-col content-center items-center justify-center rounded-2xl md:ml-12">
-        <Image src={frensLogo} alt={"logo"} height={185} width={185} />
-      </div>
-      <div className="w-full py-2 md:ml-4">
-        <div className="flex justify-between">
+    <div className="mx-2 mt-10 flex max-h-[523px] flex-col rounded-xl border-[1px] border-black2Color bg-[#FFFFFF] px-3 pb-6 pt-4 md:mt-[70px] md:flex md:h-[346px] md:p-6 md:pt-8">
+      <div className="flex flex-col items-center gap-8 rounded-2xl md:flex-row md:items-start md:gap-12">
+        <Image
+          src={frensLogo}
+          alt={"logo"}
+          height={154}
+          width={154}
+          className="md:ml-8 md:h-[124px] md:w-40"
+        />
+        <div className="flex w-full flex-1 items-start justify-between">
           <div>
-            <p className={classNames("text-3xl font-extrabold text-black2Color")}>SuiFrens</p>
-            <p className={classNames("font-light text-black2Color")}>
+            <p className={classNames("text-2xl font-extrabold text-blackColor md:text-3xl")}>
+              SuiFrens
+            </p>
+            <p className={classNames("mt-2 font-semibold text-black2Color")}>
               Each staked frens will earn 1 point per minute
             </p>
           </div>
           <button
             onClick={() => setOpenRules(true)}
-            className={classNames("text-md mr-2 font-medium text-black2Color hover:underline")}
+            className={classNames(
+              "text-md font-medium text-black2Color underline hover:no-underline",
+            )}
           >
             FAQs
           </button>
         </div>
-        <div className="mt-4 h-24 justify-between gap-4 md:flex">
-          <div className="text flex w-full content-center justify-between rounded-xl bg-[#5A5A95] px-3 py-4 text-start text-white md:w-full md:flex-col md:justify-center">
-            <p className={classNames("font-extrabold md:text-sm md:leading-4")}>
-              Total <br className="hidden md:flex" />
-              Staked
-            </p>
-            <p className={classNames("text-2xl font-black")}>{totalStaked ? totalStaked : 0}</p>
-          </div>
-          <div className="text mt-2 flex w-full content-center justify-between rounded-xl bg-pinkColor px-3 py-4 text-start text-white md:mt-0 md:w-full md:flex-col md:justify-center">
-            <p className={classNames("font-extrabold md:text-sm md:leading-4")}>
-              You <br className="hidden md:flex" />
-              Staked
-            </p>
-            <p className={classNames("text-2xl font-black")}>
-              {stakedList?.length ? stakedList.length : 0}
-            </p>
-          </div>
-          <div className="mt-2 flex w-full content-center justify-between rounded-xl bg-yellowColor px-3 py-4 text-start text-white md:mt-0 md:w-full md:flex-col md:justify-center">
-            <p className={classNames("mt-2 font-extrabold md:mt-0 md:text-sm md:leading-4")}>
+      </div>
+      <div className="mt-4 w-full flex-1 md:mt-9">
+        <div className="flex h-full flex-col gap-[10px] md:flex-row md:gap-4">
+          <div className="flex w-full  content-center items-center justify-between rounded-xl bg-yellowColor px-3 py-4 text-start text-white md:mt-0 md:h-full md:flex-col md:items-start">
+            <p className={classNames("text-[18px] font-semibold leading-[22px] md:mt-0")}>
               Your Available Hola Points
             </p>
             <div>
-              <div className="flex gap-2 md:w-full md:justify-between">
-                <p className={classNames("pt-1 text-xl font-black md:pt-0 md:text-xl")}>
+              <div className="flex content-center gap-2 md:w-full md:justify-between">
+                <p className={classNames("text-[26px] font-extrabold leading-8 md:text-3xl")}>
                   {totalHolaPointsOnchain ? formatNumber(totalHolaPointsOnchain) : 0}
                 </p>
                 {stakedList !== null && stakedList?.length !== 0 && availablePointsToClaim > 0 && (
@@ -77,6 +71,20 @@ export const ProjectCard = ({
                 )}
               </div>
             </div>
+          </div>
+          <div className="text flex w-full content-center items-center justify-between rounded-xl bg-orangeColor px-3 py-4 text-start text-white md:mt-0 md:h-full md:flex-col md:items-start">
+            <p className={classNames("text-[18px] font-semibold leading-[22px]")}>
+              You Total Staked
+            </p>
+            <p className={classNames("text-[26px] font-extrabold leading-8 md:text-3xl")}>
+              {stakedList?.length ? stakedList.length : 0}
+            </p>
+          </div>
+          <div className="text flex w-full content-center items-center justify-between rounded-xl bg-[#5A5A95] px-3 py-4 text-start text-white md:h-full md:flex-col md:items-start">
+            <p className={classNames("text-[18px] font-semibold leading-[22px]")}>Total Staked</p>
+            <p className={classNames("text-[26px] font-extrabold leading-8 md:text-3xl")}>
+              {totalStaked ? totalStaked : 0}
+            </p>
           </div>
         </div>
       </div>
