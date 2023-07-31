f5f6a50f861c2f4712fe572ea81a2f3da55d8d9b
igorlebedev <igorlebedev228@gmail.com>
2023-07-31 12:43:29 +0700

    fix font & add ticket logo available hola points

--- website/src/components/Staking/ProjectCard.tsx
+++ website/src/components/Staking/ProjectCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { classNames, formatNumber } from "utils";
 import { IProjectCard } from "types";
 import frensLogo from "/public/img/frens-logo.svg";
+import iconTicketStar from "/public/img/IconTicketStar.svg";
 
 export const ProjectCard = ({
   totalStaked,
@@ -11,8 +12,27 @@ export const ProjectCard = ({
   availablePointsToClaim,
   claimPointsFunction,
 }: IProjectCard) => {
+  // TODO: BUTTON FOR CLAIM POINT ADD LATER TO YOUH POINTS CARD
+  //   const button = {stakedList !== null && stakedList?.length !== 0 && availablePointsToClaim > 0 && (
+  //     <button
+  //       className="text-sm underline"
+  //       onClick={() => claimPointsFunction(stakedList.map((ticket) => ticket.id))}
+  //     >{`Claim ${formatNumber(availablePointsToClaim)} `}</button>
+  //   )}
+  // </div>
+  // {/* TODO: rewrite this part */}
+  // {/* //this claim is available only on desktop */}
+  // <div className="flex text-left text-xs underline md:hidden ">
+  //   {stakedList?.length && availablePointsToClaim ? (
+  //     <button
+  //       className="underline"
+  //       onClick={() => claimPointsFunction(stakedList?.map((ticket) => ticket.id))}
+  //     >{`Claim ${formatNumber(availablePointsToClaim)} `}</button>
+  //   ) : (
+  //     <button></button>
+  //   )}
   return (
-    <div className="mx-2 mt-10 flex max-h-[523px] flex-col rounded-xl border-[1px] border-black2Color bg-[#FFFFFF] px-3 pb-6 pt-4 md:mt-[70px] md:flex md:h-[346px] md:p-6 md:pt-8">
+    <div className="mx-2 mt-10 flex max-h-[523px] flex-col rounded-xl border-[1px] border-black2Color bg-[#FFFFFF] px-3 pb-6 pt-4 font-inter md:mt-[70px] md:flex md:h-[346px] md:p-6 md:pt-8">
       <div className="flex flex-col items-center gap-8 rounded-2xl md:flex-row md:items-start md:gap-12">
         <Image
           src={frensLogo}
@@ -26,7 +46,7 @@ export const ProjectCard = ({
             <p className={classNames("text-2xl font-extrabold text-blackColor md:text-3xl")}>
               SuiFrens
             </p>
-            <p className={classNames("mt-2 font-semibold text-black2Color")}>
+            <p className={classNames("mt-2 font-medium text-black2Color")}>
               Each staked frens will earn 1 point per minute
             </p>
           </div>
@@ -46,30 +66,11 @@ export const ProjectCard = ({
             <p className={classNames("text-[18px] font-semibold leading-[22px] md:mt-0")}>
               Your Available Hola Points
             </p>
-            <div>
-              <div className="flex content-center gap-2 md:w-full md:justify-between">
-                <p className={classNames("text-[26px] font-extrabold leading-8 md:text-3xl")}>
-                  {totalHolaPointsOnchain ? formatNumber(totalHolaPointsOnchain) : 0}
-                </p>
-                {stakedList !== null && stakedList?.length !== 0 && availablePointsToClaim > 0 && (
-                  <button
-                    className="text-sm underline"
-                    onClick={() => claimPointsFunction(stakedList.map((ticket) => ticket.id))}
-                  >{`Claim ${formatNumber(availablePointsToClaim)} `}</button>
-                )}
-              </div>
-              {/* TODO: rewrite this part */}
-              {/* //this claim is available only on desktop */}
-              <div className="flex text-left text-xs underline md:hidden ">
-                {stakedList?.length && availablePointsToClaim ? (
-                  <button
-                    className="underline"
-                    onClick={() => claimPointsFunction(stakedList?.map((ticket) => ticket.id))}
-                  >{`Claim ${formatNumber(availablePointsToClaim)} `}</button>
-                ) : (
-                  <button></button>
-                )}
-              </div>
+            <div className="flex content-center items-center gap-2 md:w-full">
+              <Image src={iconTicketStar} alt={"ticketLogo"} height={28} width={28} />
+              <p className={classNames("text-[26px] font-extrabold leading-8 md:text-3xl")}>
+                {totalHolaPointsOnchain ? formatNumber(totalHolaPointsOnchain) : 0}
+              </p>
             </div>
           </div>
           <div className="text flex w-full content-center items-center justify-between rounded-xl bg-orangeColor px-3 py-4 text-start text-white md:mt-0 md:h-full md:flex-col md:items-start">
