a773d676dd9c2556f6526c3451122124e30fe828
igorlebedev <igorlebedev228@gmail.com>
2023-08-01 19:07:20 +0700

    add availablepoints prop and formatNumber

--- website/src/components/Staking/PointsBanner.tsx
+++ website/src/components/Staking/PointsBanner.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import pointsLogo from "/public/img/points.png";
+import { formatNumber } from "utils";
 
-export const PointsBanner = () => {
+export const PointsBanner = ({ availablePointsToClaim }: { availablePointsToClaim: number }) => {
   return (
     <div className="mt-5 flex w-auto flex-col content-center items-center justify-between gap-5 rounded-lg bg-pinkColor p-4 pb-6 text-white md:mx-[25px] md:mt-8 md:flex-row md:py-6">
       <div className="w-full">
         <p className="mb-4 text-lg font-semibold">Your Hola Points</p>
         <div className="flex items-center gap-[6px]">
           <Image src={pointsLogo} alt={"Points Logo"} width={40} height={40} />
-          <p className="text-[26px] font-extrabold md:text-3xl">0</p>
+          <p className="text-[26px] font-extrabold md:text-3xl">
+            {formatNumber(availablePointsToClaim)}
+          </p>
         </div>
       </div>
       <div className="flex w-full flex-col items-start justify-center gap-4 md:items-center lg:flex-row lg:gap-8">
