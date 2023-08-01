3436a8c92ad61e6879d45632895f74af58c4ab34
igorlebedev <igorlebedev228@gmail.com>
2023-08-01 11:56:42 +0700

    minor fixes & My nfts buttons staking fix

--- website/src/layouts/StakingLayout/StakingLayout.tsx
+++ website/src/layouts/StakingLayout/StakingLayout.tsx
@@ -26,6 +26,7 @@ import {
   UnstakeDetailDialog,
   Container,
 } from "components";
+import classnames from "classnames";
 import { getExecutionStatus, getExecutionStatusError, getObjectFields } from "@mysten/sui.js";
 import { Montserrat } from "next/font/google";
 
@@ -335,6 +336,7 @@ export const StakingLayout = () => {
             : setOpenedFrend(true);
           setSelectedFrend(capy);
         }}
+        className="proposal-card-shadow hover:bg-red rounded-xl border-2 border-blackColor"
       >
         <div
           className={classNames(
@@ -395,7 +397,7 @@ export const StakingLayout = () => {
   return status === EthosConnectStatus.NoConnection ? (
     <NoConnectWallet title={"Staking!"} />
   ) : (
-    <Container>
+    <Container className="font-inter">
       {stakedFrens ? (
         <ProjectCard
           availablePointsToClaim={availablePointsToClaim}
@@ -412,27 +414,16 @@ export const StakingLayout = () => {
       {stakedFrens?.length !== 0 && (
         <>
           <div className="flex justify-between">
-            <h1
-              className={classNames(
-                "mt-8 text-xl font-semibold text-black2Color md:text-4xl",
-                font_montserrat.className,
-              )}
-            >
-              My Staked Frens
+            <h1 className={classnames("text-[26px] font-extrabold text-blackColor lg:text-3xl")}>
+              My Staked NFTs
             </h1>
             <div className="md:flex">
-              <p
-                className={classNames(
-                  "mt-9 w-full text-xs font-normal md:mt-10 md:px-4 md:text-sm",
-                )}
-              >
+              <p className={classNames("mt-9 w-full text-xs font-normal md:mt-10 md:px-4")}>
                 {batchUnstakeMode ? (
                   batchIdUnstake.length === 0 ? (
                     "Select capy for unstaking"
                   ) : (
-                    <div className="-mt-1">
-                      <StakingRules />
-                    </div>
+                    <StakingRules />
                   )
                 ) : null}
               </p>
@@ -460,7 +451,7 @@ export const StakingLayout = () => {
               </button>
             </div>
           </div>
-          <div className={"mt-8 grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-10"}>
+          <div className={"mt-8 grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-10 xl:grid-cols-4"}>
             {stakedFrens?.map((stack) => (
               <StakedTicketCard staking={stack} key={stack.id} batchMode={batchUnstakeMode} />
             ))}
@@ -468,36 +459,30 @@ export const StakingLayout = () => {
         </>
       )}
 
-      <div className="flex justify-between">
-        <h1
-          className={classNames(
-            "mt-8 text-xl font-semibold text-black2Color md:text-4xl",
-            font_montserrat.className,
-          )}
-        >
-          My Frens
+      <div className="flex flex-col justify-between md:flex-row">
+        <h1 className={classNames("text-[26px] font-extrabold text-blackColor lg:text-3xl")}>
+          My NFTs
         </h1>
         {frens?.length !== 0 ? (
-          <>
-            <div className="md:flex">
-              <p
-                className={classNames(
-                  "mt-9 w-full text-xs font-normal md:mt-10 md:px-4 md:text-sm",
-                )}
-              >
-                {batchStakeMode ? (
-                  batchIdStake.length === 0 ? (
-                    "Select capy for staking"
-                  ) : (
-                    <div className="-mt-1">
-                      <StakingRules />
-                    </div>
-                  )
-                ) : null}
-              </p>
+          <div className="mt-4 md:mt-0 md:flex">
+            {/* TODO: REMOVE OR REWORK */}
+            <p
+              className={classnames("mb-2 w-full text-sm font-normal md:mb-0 md:px-4 md:text-sm", {
+                hidden: !batchStakeMode,
+              })}
+            >
+              {batchStakeMode ? (
+                batchIdStake.length === 0 ? (
+                  "Select capy for staking"
+                ) : (
+                  <StakingRules />
+                )
+              ) : null}
+            </p>
+            <div className="flex gap-4 md:gap-5">
               <button
                 className={classNames(
-                  "w-full rounded-xl border-2 border-yellowColor bg-white px-3 py-4 text-sm text-yellowColor hover:border-transparent hover:bg-yellowColor hover:text-gray-50 md:mt-8 md:min-w-[220px] md:px-8 md:py-2 md:text-lg",
+                  "button-shadow button-shadow:active w-full rounded-xl border-2 border-yellowColor bg-white px-3 py-4 text-lg font-semibold text-yellowColor hover:border-transparent hover:bg-yellowColor hover:text-gray-50 md:min-w-[176px] md:px-6 md:py-3",
                 )}
                 onClick={() => {
                   batchStakeMode
@@ -513,14 +498,29 @@ export const StakingLayout = () => {
                     : "Confirm"
                   : "Batch Staking"}
               </button>
+              {/* TODO: Change to "stake all" button. Now it's example */}
+              <button
+                className={classNames(
+                  "button-shadow button-shadow:active w-full rounded-xl border-2 border-blackColor bg-yellowColor px-3 py-4 text-lg font-semibold text-white hover:bg-white hover:text-yellowColor md:min-w-[176px] md:px-6 md:py-3",
+                )}
+                onClick={() => {
+                  batchStakeMode
+                    ? batchIdStake.length === 0
+                      ? setBatchStakeMode(false)
+                      : stakeBatchCapy(batchIdStake)
+                    : setBatchStakeMode(true);
+                }}
+              >
+                {batchStakeMode ? (batchIdStake.length === 0 ? "Cancel" : "Confirm") : "Stake all"}
+              </button>
             </div>
-          </>
+          </div>
         ) : (
           <></>
         )}
       </div>
       {frens?.length !== 0 ? (
-        <div className={"mt-8 grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-10"}>
+        <div className={"mt-8 grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-10 xl:grid-cols-4"}>
           {frens?.map((capy) => (
             <SuifrensCard capy={capy} key={capy.id} batchMode={batchStakeMode} />
           ))}
@@ -532,7 +532,7 @@ export const StakingLayout = () => {
               <div
                 className={classNames(
                   font_montserrat.className,
-                  "text-2xl font-semibold text-black2Color md:text-4xl",
+                  "text-lg font-medium text-black2Color",
                 )}
               >
                 All your capies are staked
@@ -541,8 +541,8 @@ export const StakingLayout = () => {
             </div>
           ) : (
             <div className={classNames("mt-8 text-center", font_montserrat.className)}>
-              <div className={classNames("text-4xl font-semibold text-black2Color")}>
-                You don't have capy :(
+              <div className={classNames("text-lg font-medium text-black2Color")}>
+                Sorry, SuiFrens hasn’t yet added NFTs for staking
               </div>
               <BlueMoveButton text={"Get one Capy on"} />
             </div>
