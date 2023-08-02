27569fd944d7ae0b29bf69ca6c7fbb3725a56d6c
igorlebedev <igorlebedev228@gmail.com>
2023-08-02 17:43:21 +0700

    remove useless margin

--- website/src/components/Skeleton/SkeletonStakingProjectCard.tsx
+++ website/src/components/Skeleton/SkeletonStakingProjectCard.tsx
@@ -1,5 +1,5 @@
 export const SkeletonStakingProjectCard = () => {
   return (
-    <div className="flex max-h-[547px] min-h-[547px] animate-pulse flex-col rounded-xl border-[1px] border-black2Color bg-gray2Color px-3 pb-6 pt-4 font-inter md:flex md:max-h-[483px] md:min-h-[483px] md:p-[25px] md:pt-[30px] lg:mt-[70px] lg:max-h-[365px] lg:min-h-[365px]"></div>
+    <div className="flex max-h-[547px] min-h-[547px] animate-pulse flex-col rounded-xl border-[1px] border-black2Color bg-gray2Color px-3 pb-6 pt-4 font-inter md:flex md:max-h-[483px] md:min-h-[483px] md:p-[25px] md:pt-[30px] lg:max-h-[365px] lg:min-h-[365px]"></div>
   );
 };
