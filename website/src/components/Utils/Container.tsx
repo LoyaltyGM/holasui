483d4a8d0559aeb07d325abcb110df750dff87ab
igorlebedev <igorlebedev228@gmail.com>
2023-08-03 19:28:56 +0700

    fix container sizing for every screen

--- website/src/components/Utils/Container.tsx
+++ website/src/components/Utils/Container.tsx
@@ -9,7 +9,7 @@ interface ContainerProps {
 export const Container: FC<ContainerProps> = ({ children, className }): React.ReactNode => (
   <main
     className={cn(
-      "z-10 mt-[100px] flex min-h-[85vh] flex-col rounded-lg bg-basicColor px-4 md:mt-[130px] md:px-10 lg:mt-40 lg:px-16 xl:px-[152px]",
+      "z-10 mt-[100px] flex min-h-[85vh] flex-col rounded-lg bg-basicColor px-4 md:mt-[116px] md:px-10 lg:mt-[146px] lg:px-16 xl:mt-40 xl:px-[152px]",
       className,
     )}
   >
