0d419ae8835e543ed770e4f34179163a2b544c19
igorlebedev <igorlebedev228@gmail.com>
2023-08-09 17:15:26 +0700

    minor fix

--- website/src/components/Spaces/SpaceCard.tsx
+++ website/src/components/Spaces/SpaceCard.tsx
@@ -3,7 +3,7 @@ import frensLogo from "/public/img/frens-logo.svg";
 
 export const SpaceCard = () => {
   return (
-    <div className="card-shadow flex w-full gap-5 rounded-[20px] border border-blackColor bg-white p-[15px] text-blackColor">
+    <div className="card-shadow flex max-h-[180px] w-full gap-5 rounded-[20px] border border-blackColor bg-white p-[15px] text-blackColor">
       <div className="flex h-[150px] w-[150px] items-center">
         <Image
           src={frensLogo}
@@ -19,11 +19,11 @@ export const SpaceCard = () => {
           <p className="font-medium">10 quests</p>
         </div>
         {/* TODO: Separate to the other component LINK or make it with tailwind classes?? */}
-        <button className="group h-[30px] w-[88px] transition-all">
-          <p className="font-medium leading-5 text-purpleColor group-hover:text-blackColor group-active:text-purpleColorHover group-disabled:text-grayColor">
+        <button className="group h-[30px] w-[88px]">
+          <p className="font-medium leading-5 text-purpleColor duration-200 group-hover:text-blackColor group-active:text-purpleColorHover group-disabled:text-grayColor">
             Learn more
           </p>
-          <div className="h-[2px] w-full rounded-[10px] bg-purpleColor group-hover:bg-blackColor group-active:bg-purpleColorHover group-disabled:bg-grayColor" />
+          <div className="h-[2px] w-full rounded-[10px] bg-purpleColor duration-200 group-hover:bg-blackColor group-active:bg-purpleColorHover group-disabled:bg-grayColor" />
         </button>
       </div>
     </div>
