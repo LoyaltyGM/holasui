71cb5816240dc88bd76134d4387ed459e7a867de
igorlebedev <igorlebedev228@gmail.com>
2023-08-10 16:26:26 +0700

    rename

--- website/src/components/Spaces/index.ts
+++ website/src/components/Spaces/index.ts
@@ -1,5 +1,5 @@
 export * from "./SpaceInfoBanner";
 export * from "./JourneyCard";
 export * from "./QuestCard";
-export * from "./MainSpaceCard";
+export * from "./PromotedCard";
 export * from "./SpaceCard";
