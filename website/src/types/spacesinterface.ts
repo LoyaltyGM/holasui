8dfd8394a9841d593ee52917f0daa85d4bbdae56
igorlebedev <igorlebedev228@gmail.com>
2023-08-11 23:12:07 +0700

    add interfaces

--- website/src/types/spacesinterface.ts
+++ website/src/types/spacesinterface.ts
@@ -3,4 +3,27 @@ export interface ISpaces {
   name: string;
   description: string;
   image: string;
+  total_completed: number;
+  start_time: number;
+  end_time: number;
+  points: number;
+  journeys: IJourney[] | null;
+}
+
+export interface IJourney {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  start_time: number;
+  end_time: number;
+  quests: IQuest[] | null;
+}
+
+export interface IQuest {
+  id: string;
+  points_amount: number;
+  name: string;
+  description: string;
+  total_completed: number;
 }
