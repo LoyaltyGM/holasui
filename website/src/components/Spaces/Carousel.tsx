7c34a0888098248e85cd33c6c8d5856586c47437
igorlebedev <igorlebedev228@gmail.com>
2023-08-14 18:03:56 +0700

    add carousel

---
+++ website/src/components/Spaces/Carousel.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+
+const MD_WIDTH = 768;
+const LG_WIDTH = 976;
+const MD_GAP = 16;
+const LG_GAP = 24;
+
+const useWidth = (): number => {
+  const [width, setWindowDimensions] = useState<number>(0);
+  useEffect(() => {
+    function handleResize(): void {
+      setWindowDimensions(window.innerWidth);
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return (): void => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return width;
+};
+
+export const Carousel = ({ children }: { children: JSX.Element[] }) => {
+  const width = useWidth();
+  const [curPosition, setCurPosition] = useState<number>(0);
+  const [maxWidth, setMaxWidth] = useState<number>(0);
+  const [curScreen, setCurScreen] = useState<string | null>(null);
+  const [currentCardIdx, setCurrentCardIdx] = useState<number>(0);
+
+  useEffect(() => {
+    let newCurrentScreen = "";
+    let newMaxWidth = 0;
+    if (!Array.isArray(children)) {
+      setMaxWidth(0);
+    } else if (width >= MD_WIDTH && width < LG_WIDTH) {
+      newCurrentScreen = "medium";
+      newMaxWidth = (children.length - 1) * 600 + (children.length - 1) * MD_GAP;
+    } else if (width >= LG_WIDTH) {
+      newCurrentScreen = "large";
+      newMaxWidth = (children.length - 1) * 700 + (children.length - 1) * LG_GAP;
+    }
+    if (newCurrentScreen !== curScreen) {
+      const calculatePosition = () => {
+        return (newMaxWidth / (children.length - 1)) * currentCardIdx;
+      };
+      setCurPosition(-calculatePosition());
+      setCurScreen(newCurrentScreen);
+      setMaxWidth(newMaxWidth);
+    }
+  }, [width]);
+
+  const slideLeft = () => {
+    if (curPosition <= -maxWidth) {
+      return;
+    } else if (width >= MD_WIDTH && width < LG_WIDTH) {
+      setCurrentCardIdx(currentCardIdx + 1);
+      setCurPosition(curPosition - 616);
+    } else if (width >= LG_WIDTH) {
+      setCurrentCardIdx(currentCardIdx + 1);
+      setCurPosition(curPosition - 724);
+    }
+  };
+  const slideRight = () => {
+    if (curPosition >= 0) {
+      return;
+    } else if (width >= MD_WIDTH && width < LG_WIDTH) {
+      setCurrentCardIdx(currentCardIdx - 1);
+      setCurPosition(curPosition + 616);
+    } else if (width >= LG_WIDTH) {
+      setCurrentCardIdx(currentCardIdx - 1);
+      setCurPosition(curPosition + 724);
+    }
+  };
+
+  const ArrowIconLeft = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M12.5 4.16669L7.5 10L12.5 15.8334"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="stroke-blackColor"
+      />
+    </svg>
+  );
+  const ArrowIconRight = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M7.5 4.16669L12.5 10L7.5 15.8334"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="stroke-blackColor"
+      />
+    </svg>
+  );
+  return (
+    <div className="h-max">
+      <div className="mb-5 hidden justify-end gap-2 md:flex">
+        <button
+          onClick={slideRight}
+          disabled={currentCardIdx === 0}
+          className="flex h-[30px] w-10 items-center justify-center rounded-lg border border-blackColor bg-white"
+        >
+          <ArrowIconLeft />
+        </button>
+        <button
+          onClick={slideLeft}
+          disabled={currentCardIdx === children.length - 1}
+          className="flex h-[30px] w-10 items-center justify-center rounded-lg border border-blackColor bg-white"
+        >
+          <ArrowIconRight />
+        </button>
+      </div>
+      <div
+        className="hide-scroll-bar flex h-max gap-4 overflow-x-auto overflow-y-visible transition-all duration-500  md:overflow-visible lg:gap-6"
+        style={{ transform: `translateX(${curPosition}px)` }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
