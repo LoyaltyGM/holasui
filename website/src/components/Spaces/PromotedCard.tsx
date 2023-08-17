8c8ed0106fa6a7b29373190dd02ec8bde61970dd
igorlebedev <igorlebedev228@gmail.com>
2023-08-17 19:44:19 +0700

    minor fix interface and image

--- website/src/components/Spaces/PromotedCard.tsx
+++ website/src/components/Spaces/PromotedCard.tsx
@@ -5,27 +5,26 @@ import { Button } from "components";
 interface IPromotedCard {
   title: string;
   description: string;
-  imageUrl: string;
-  spaceAddress: string;
+  image_url: string;
+  spaceAddress: number;
 }
 
 export const PromotedCard = (props: IPromotedCard) => {
   const CompanyImage = () => {
     return (
-      <div className="mb-4 flex h-[130px] justify-center lg:h-[140px] xl:min-w-[170px]">
+      <div className="relative mx-auto mb-4 flex h-[130px] w-[130px] justify-center rounded-full lg:h-[140px] lg:w-[140px] xl:mx-0 xl:h-[170px] xl:w-[170px]">
         <Image
-          src={props.imageUrl}
+          src={props.image_url}
           alt={"Company image"}
-          height={130}
-          width={130}
-          className="rounded-full text-center lg:h-[140px] lg:w-[140px] xl:h-[170px] xl:w-[170px]"
+          fill
+          className="rounded-full object-contain"
         />
       </div>
     );
   };
   const CompanyInfo = () => {
     return (
-      <div>
+      <div className="flex flex-col justify-between">
         <div className="mb-5">
           <h2 className="mb-3 text-lg font-semibold lg:text-[22px]">Journeys from {props.title}</h2>
           <p className="font-medium text-black2Color">{props.description}</p>
@@ -39,7 +38,7 @@ export const PromotedCard = (props: IPromotedCard) => {
     );
   };
   return (
-    <div className="w-full rounded-xl border border-blackColor bg-white px-4 py-5 lg:p-5 xl:flex xl:flex-row-reverse xl:gap-4">
+    <div className="w-full rounded-xl border border-blackColor bg-white px-4 py-5 lg:p-5 xl:flex xl:min-h-[241px] xl:flex-row-reverse xl:justify-between xl:gap-4">
       <CompanyImage />
       <CompanyInfo />
     </div>
