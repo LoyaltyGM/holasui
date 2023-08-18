bd21af60fa94a35d76b3b8e0c0a7a8665304125f
igorlebedev <igorlebedev228@gmail.com>
2023-08-18 17:56:09 +0700

    fix description and title size & overflow

--- website/src/components/Spaces/PromotedCard.tsx
+++ website/src/components/Spaces/PromotedCard.tsx
@@ -12,7 +12,7 @@ interface IPromotedCard {
 export const PromotedCard = (props: IPromotedCard) => {
   const CompanyImage = () => {
     return (
-      <div className="relative mx-auto mb-4 flex h-[130px] w-[130px] justify-center rounded-full lg:h-[140px] lg:w-[140px] xl:mx-0 xl:h-[170px] xl:w-[170px]">
+      <div className="relative mx-auto mb-4 flex min-h-[130px] min-w-[130px] justify-center rounded-full lg:min-h-[140px] lg:min-w-[140px] xl:mx-0 xl:min-h-[170px] xl:min-w-[170px]">
         <Image
           src={props.image_url}
           alt={"Company image"}
@@ -25,9 +25,14 @@ export const PromotedCard = (props: IPromotedCard) => {
   const CompanyInfo = () => {
     return (
       <div className="flex flex-col justify-between">
-        <div className="mb-5">
-          <h2 className="mb-3 text-lg font-semibold lg:text-[22px]">Journeys from {props.title}</h2>
-          <p className="font-medium text-black2Color">{props.description}</p>
+        {/* TODO: fix overflow title width. description overflow height. maybe add dynamic height of promoted card */}
+        <div className="mb-5 xl:w-[330px]">
+          <h2 className="max-w mb-3 overflow-hidden text-ellipsis whitespace-nowrap text-lg font-semibold leading-[22px] lg:text-[22px]">
+            Journeys from {props.title}
+          </h2>
+          <p className="overflow-hidden text-ellipsis font-medium leading-[1.4] text-black2Color xl:max-h-[88px]">
+            {props.description}
+          </p>
         </div>
         <Link href={`spaces/${props.spaceAddress}`}>
           <Button btnType="button" variant="button-primary-puprle" size="sm-full">
@@ -38,7 +43,7 @@ export const PromotedCard = (props: IPromotedCard) => {
     );
   };
   return (
-    <div className="w-full rounded-xl border border-blackColor bg-white px-4 py-5 lg:p-5 xl:flex xl:min-h-[241px] xl:flex-row-reverse xl:justify-between xl:gap-4">
+    <div className="w-full rounded-xl border border-blackColor bg-white px-4 py-5 lg:p-5 xl:flex xl:max-h-[241px] xl:min-h-[241px] xl:flex-row-reverse xl:justify-between xl:gap-4">
       <CompanyImage />
       <CompanyInfo />
     </div>
