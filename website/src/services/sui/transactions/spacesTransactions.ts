df63e3f13beb7777126f60a7378e367340f5df13
igorlebedev <igorlebedev228@gmail.com>
2023-08-29 20:09:34 +0700

    add create quest func and edit space func

--- website/src/services/sui/transactions/spacesTransactions.ts
+++ website/src/services/sui/transactions/spacesTransactions.ts
@@ -1,5 +1,9 @@
 import { TransactionBlock } from "@mysten/sui.js";
+import { space } from "postcss/lib/list";
 import { SPACE_HUB_ID, SPACE_PACKAGE } from "utils";
+import { suiProvider } from "../suiProvider";
+
+// ==== SPACES ====
 
 export const signTransactionCreateSpace = ({
   name,
@@ -30,6 +34,59 @@ export const signTransactionCreateSpace = ({
   return tx;
 };
 
+export const signTransactionEditSpace = ({
+  admin_cap,
+  space,
+  name,
+  description,
+  image_url,
+  twitter_url,
+  website_url,
+}: {
+  admin_cap: string;
+  space: string;
+  name?: string;
+  description?: string;
+  image_url?: string;
+  twitter_url?: string;
+  website_url?: string;
+}) => {
+  const tx = new TransactionBlock();
+  if (name) {
+    tx.moveCall({
+      target: `${SPACE_PACKAGE}::quest::update_space_name`,
+      arguments: [tx.pure(admin_cap), tx.pure(space), tx.pure(name)],
+    });
+  }
+  if (description) {
+    tx.moveCall({
+      target: `${SPACE_PACKAGE}::quest::update_space_description`,
+      arguments: [tx.pure(admin_cap), tx.pure(space), tx.pure(description)],
+    });
+  }
+  if (image_url) {
+    tx.moveCall({
+      target: `${SPACE_PACKAGE}::quest::update_space_image_url`,
+      arguments: [tx.pure(admin_cap), tx.pure(space), tx.pure(image_url)],
+    });
+  }
+  if (twitter_url) {
+    tx.moveCall({
+      target: `${SPACE_PACKAGE}::quest::update_space_twitter_url`,
+      arguments: [tx.pure(admin_cap), tx.pure(space), tx.pure(twitter_url)],
+    });
+  }
+  if (website_url) {
+    tx.moveCall({
+      target: `${SPACE_PACKAGE}::quest::update_space_website_url`,
+      arguments: [tx.pure(admin_cap), tx.pure(space), tx.pure(website_url)],
+    });
+  }
+  return tx;
+};
+
+// ==== JOURNEYS ====
+
 export const signTransactionCreateJourney = ({
   admin_cap,
   space,
@@ -52,8 +109,8 @@ export const signTransactionCreateJourney = ({
   end_time: number;
 }) => {
   const tx = new TransactionBlock();
+  // TODO: fix gas
   const [coin] = tx.splitCoins(tx.gas, [tx.pure(0, "u64")]);
-  console.log(coin);
   tx.moveCall({
     target: `${SPACE_PACKAGE}::quest::create_journey`,
     arguments: [
@@ -68,6 +125,75 @@ export const signTransactionCreateJourney = ({
       tx.pure(description),
       tx.pure(start_time),
       tx.pure(end_time),
+    ],
+  });
+  return tx;
+};
+
+// TODO: rework
+// export const getJourney = async ({
+//   sender = "0xbfa592bb480ab82022b716ee1d754b36799d9890559620b64c30127695c87c13",
+//   space_id,
+//   journey_id,
+// }: {
+//   sender?: string;
+//   space_id: string;
+//   journey_id: string;
+// }) => {
+//   const tx = new TransactionBlock();
+//   tx.moveCall({
+//     target: `${SPACE_PACKAGE}::quest::journey`,
+//     arguments: [tx.pure(space_id), tx.pure(journey_id)],
+//   });
+//   const response = await suiProvider.devInspectTransactionBlock({
+//     transactionBlock: tx,
+//     sender,
+//   });
+//   console.log("respones", response.results);
+// };
+
+// ==== QUESTS ====
+
+export const signTransactionCreateQuest = ({
+  admin_cap,
+  space,
+  journey_id,
+  points_amount,
+  name,
+  description,
+  call_to_action_url,
+  package_id,
+  module_name,
+  function_name,
+  func_arguments,
+}: {
+  admin_cap: string;
+  space: string;
+  journey_id: string;
+  points_amount: number;
+  name: string;
+  description: string;
+  call_to_action_url: string;
+  package_id: string;
+  module_name: string;
+  function_name: string;
+  func_arguments: string[];
+}) => {
+  const tx = new TransactionBlock();
+  tx.moveCall({
+    target: `${SPACE_PACKAGE}::quest::create_quest`,
+    arguments: [
+      tx.pure(admin_cap),
+      tx.pure(space),
+      tx.pure(journey_id),
+      tx.pure(points_amount),
+      tx.pure(name),
+      tx.pure(description),
+      tx.pure(call_to_action_url),
+      tx.pure(package_id),
+      tx.pure(module_name),
+      tx.pure(function_name),
+      tx.pure(func_arguments),
     ],
   });
   return tx;
