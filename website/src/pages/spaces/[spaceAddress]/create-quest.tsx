9d8654b2846dad0c28da0e27af28c1d5c0cf4cb0
igorlebedev <igorlebedev228@gmail.com>
2023-08-31 20:13:16 +0700

    add form and contract logic for create quest

--- website/src/pages/spaces/[spaceAddress]/create-quest.tsx
+++ website/src/pages/spaces/[spaceAddress]/create-quest.tsx
@@ -1,7 +1,31 @@
 import { CreateQuestLayout } from "layouts";
 
-const CreateQuest = () => {
-  return <CreateQuestLayout />;
+import { GetServerSideProps, NextPage } from "next";
+
+interface ISpaceAddressProps {
+  spaceAddress: string;
+}
+
+export const getServerSideProps: GetServerSideProps<ISpaceAddressProps> = async ({ params }) => {
+  try {
+    const spaceAddress = params?.spaceAddress as string;
+    return {
+      props: {
+        spaceAddress,
+      },
+    };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+    };
+  }
+};
+
+const CreateQuest: NextPage<ISpaceAddressProps> = ({ spaceAddress }) => {
+  return <CreateQuestLayout spaceAddress={spaceAddress} />;
 };
 
 export default CreateQuest;
