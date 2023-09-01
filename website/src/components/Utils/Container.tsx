94fe0a66d5894b65a726f1eb6bc7610805515f70
igorlebedev <igorlebedev228@gmail.com>
2023-09-01 21:44:54 +0700

    add font-inter for container

--- website/src/components/Utils/Container.tsx
+++ website/src/components/Utils/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 }
 
 export const Container: FC<ContainerProps> = ({ children, className }): React.ReactNode => (
-  <div className="z-10 flex justify-center">
+  <div className="z-10 flex justify-center font-inter">
     <main
       className={cn(
         "mt-[100px] flex min-h-[85vh] max-w-[120rem] flex-1 flex-col bg-basicColor px-4 md:mt-[116px] md:px-10 lg:mt-[146px] lg:px-16 xl:mt-40 xl:px-[152px]",
