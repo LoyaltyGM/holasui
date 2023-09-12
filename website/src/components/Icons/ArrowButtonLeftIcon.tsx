734c7636363207039c4b1b29bed32bede59a1486
igorlebedev <igorlebedev228@gmail.com>
2023-09-12 22:10:46 +0700

    Minor svg fix

--- website/src/components/Icons/ArrowButtonLeftIcon.tsx
+++ website/src/components/Icons/ArrowButtonLeftIcon.tsx
@@ -2,9 +2,9 @@ export const ArrowButtonIconLeft = ({ disabled }: { disabled: boolean }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
     <path
       d="M12.5 4.16669L7.5 10L12.5 15.8334"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={disabled ? "stroke-grayColor" : "stroke-blackColor"}
     />
   </svg>
