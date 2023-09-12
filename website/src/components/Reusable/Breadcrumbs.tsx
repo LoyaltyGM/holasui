3d3be9e68424bb17aa96206502d0a29d3b17c1e3
igorlebedev <igorlebedev228@gmail.com>
2023-09-12 22:14:09 +0700

    rework

--- website/src/components/Reusable/Breadcrumbs.tsx
+++ website/src/components/Reusable/Breadcrumbs.tsx
@@ -19,97 +19,90 @@ export const Breadcrumbs = ({
     hrefs.push(hrefParts.slice(0, i + 1).join("/"));
   }
 
+  const Skeleton = () => (
+    <li aria-current="page">
+      <div className={cn("flex items-center", isJourneyColor ? "text-white" : "text-black2Color")}>
+        <p
+          className={cn(
+            "font-semibold md:ml-2 md:mr-2",
+            isJourneyColor ? "text-gray2Color" : "text-grayColor",
+          )}
+        >
+          /
+        </p>
+        <FolderIcon className="mr-1.5 h-4 w-4" />
+        <div className="h-3 w-20 animate-pulse rounded-2xl bg-grayColor" />
+      </div>
+    </li>
+  );
+
+  const HomePage = ({ page }: { page: string }) => (
+    <li aria-current="page">
+      <Link
+        href={`/${hrefs[0]}`}
+        className={cn(
+          "inline-flex items-center",
+          isJourneyColor
+            ? "text-gray2Color hover:text-white"
+            : "text-grayColor hover:text-black2Color",
+        )}
+      >
+        <HomeIcon className="mr-1.5 h-4 w-4 fill-current" />
+        {page}
+      </Link>
+    </li>
+  );
+
+  const CurrentPage = ({ page }: { page: string }) => (
+    <li aria-current="page">
+      <div className={cn("flex items-center", isJourneyColor ? "text-white" : "text-black2Color")}>
+        <p
+          className={cn(
+            "font-semibold md:ml-2 md:mr-2",
+            isJourneyColor ? "text-gray2Color" : "text-grayColor",
+          )}
+        >
+          /
+        </p>
+        <FolderIcon className="mr-1.5 h-4 w-4" />
+        <span>{page}</span>
+      </div>
+    </li>
+  );
+
+  const PreviousPage = ({ page, idx }: { page: string; idx: number }) => (
+    <li aria-current="page">
+      <Link
+        href={`/${hrefs[idx]}`}
+        className={cn(
+          "inline-flex items-center",
+          isJourneyColor
+            ? "text-gray2Color hover:text-white"
+            : "text-grayColor hover:text-black2Color",
+        )}
+      >
+        <p
+          className={cn(
+            "font-semibold md:ml-2 md:mr-2",
+            isJourneyColor ? "text-gray2Color" : "text-grayColor",
+          )}
+        >
+          /
+        </p>
+        <FolderIcon className="mr-1.5 h-4 w-4 fill-current" />
+        <p>{page}</p>
+      </Link>
+    </li>
+  );
+
   return (
     <nav className="mb-[70px] hidden font-medium lg:flex" aria-label="Breadcrumbs">
       <ol className="inline-flex items-center">
         {pages.map((page, idx) => {
-          if (page === "undefined") {
-            return (
-              <div
-                key={idx}
-                className={cn(
-                  "flex items-center",
-                  isJourneyColor ? "text-white" : "text-black2Color",
-                )}
-              >
-                <p
-                  className={cn(
-                    "font-semibold md:ml-2 md:mr-2",
-                    isJourneyColor ? "text-gray2Color" : "text-grayColor",
-                  )}
-                >
-                  /
-                </p>
-                <FolderIcon className="mr-1.5 h-4 w-4" />
-                <div className="h-3 w-20 animate-pulse rounded-2xl bg-grayColor" />
-              </div>
-            );
-          }
-          if (idx === 0) {
-            return (
-              <li className="inline-flex items-center" key={idx}>
-                <Link
-                  href={`/${hrefs[0]}`}
-                  className={cn(
-                    "inline-flex items-center",
-                    isJourneyColor
-                      ? "text-gray2Color hover:text-white"
-                      : "text-grayColor hover:text-black2Color",
-                  )}
-                >
-                  <HomeIcon className="mr-1.5 h-4 w-4 fill-current" />
-                  {page}
-                </Link>
-              </li>
-            );
-          }
-          if (idx === pages.length - 1) {
-            return (
-              <li aria-current="page" key={idx}>
-                <div
-                  className={cn(
-                    "flex items-center",
-                    isJourneyColor ? "text-white" : "text-black2Color",
-                  )}
-                >
-                  <p
-                    className={cn(
-                      "font-semibold md:ml-2 md:mr-2",
-                      isJourneyColor ? "text-gray2Color" : "text-grayColor",
-                    )}
-                  >
-                    /
-                  </p>
-                  <FolderIcon className="mr-1.5 h-4 w-4" />
-                  <span>{page}</span>
-                </div>
-              </li>
-            );
-          }
-          return (
-            <li className="inline-flex items-center" key={idx}>
-              <Link
-                href={`/${hrefs[idx]}`}
-                className={cn(
-                  "inline-flex items-center",
-                  isJourneyColor
-                    ? "text-gray2Color hover:text-white"
-                    : "text-grayColor hover:text-black2Color",
-                )}
-              >
-                <p
-                  className={cn(
-                    "font-semibold md:ml-2 md:mr-2",
-                    isJourneyColor ? "text-gray2Color" : "text-grayColor",
-                  )}
-                >
-                  /
-                </p>
-                <FolderIcon className="mr-1.5 h-4 w-4 fill-current" />
-                <p>{page}</p>
-              </Link>
-            </li>
-          );
+          if (page === "undefined") return <Skeleton key={idx} />;
+          if (idx === 0) return <HomePage page={page} key={idx} />;
+          if (idx === pages.length - 1) return <CurrentPage page={page} key={idx} />;
+          return <PreviousPage page={page} idx={idx} key={idx} />;
         })}
       </ol>
     </nav>
