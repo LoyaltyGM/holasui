22e918ec824d2141663dd4e729e525ef9a6a015d
igorlebedev <igorlebedev228@gmail.com>
2023-09-13 22:47:02 +0700

    add skeleton journey quest

--- website/src/components/Skeleton/index.ts
+++ website/src/components/Skeleton/index.ts
@@ -2,3 +2,4 @@ export * from "./SkeletonDAO";
 export * from "./SkeletonStakingProjectCard";
 export * from "./SkeletonPromotedSpace";
 export * from "./SkeletonSpaceDetails";
+export * from "./SkeletonJourneyQuest";
